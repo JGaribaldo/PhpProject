f118eeef554b265fa3738e6614a0744d95567944
saisoft <saisoft@php-linux>
2015-02-25 01:15:39 -0500

    Initial commit

--- helloworld.php
+++ helloworld.php
@@ -6,3 +6,19 @@
  * and open the template in the editor.
  */
 
+$weather = $_GET["weather"];
+$temp = $GET["temperature"];
+
+if ($weather == "sunny" && $temp > 70){
+    echo 'Hello World!! :):):):)';
+}
+else if ($weather == "sunny" && $temp <= 70){
+    echo 'Hello World!! :)';
+}
+else if ($weather == "cloudy"){
+    echo 'Hellow World!! :|';
+}
+else {
+    echo 'Hello World :(';
+}
+?>
